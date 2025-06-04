603933c8e02ede56374faeb17c8d7a51fc3228ab
Jun-T-git <jun0303t@icloud.com>
2025-06-04 23:13:57 +0900

    リンク修正

--- src/components/molecules/AnswerFormSubmitted.tsx
+++ src/components/molecules/AnswerFormSubmitted.tsx
@@ -77,7 +77,7 @@ export const AnswerFormSubmitted: FC<AnswerFormSubmittedProps> = ({
       </div>
       <div className="flex flex-col sm:flex-row gap-4 justify-center">
         <LinkButton
-          href={`/guest/${token}/answer/edit/${editToken}`}
+          href={`/guest/${token}/summary`}
           className="w-full sm:max-w-sm px-4 py-3 text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 rounded-md shadow-sm transition-colors"
         >
           一覧画面へ進む
